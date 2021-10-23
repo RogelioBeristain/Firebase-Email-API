7f5c8f81d4fa72ff20aa4ffffe242ba7d620f6b4
Rogelio Beristain <Rogelio26.dev@gmail.com>
2021-10-23 09:01:17 -0500

    fix:por ma ñana a ver que pdo

--- functions/index.js
+++ functions/index.js
@@ -1,72 +1,75 @@
 const functions = require("firebase-functions");
+const admin = requiere('firebase-admin');
 const nodemailer = require("nodemailer");
-const express = require("express");
-const cors = require("cors");
+const cors = require("cors")({origin: true});
 
-if(process.env.NODE_ENV != 'production'){
-    require('dotenv').config();
+
+const files = {
+    'file1': {
+        filename:'MateralPro.doc',
+        path:'doc-test/Doc1.docx'
+    },
+    'file2': {
+        filename:'MateralPro.doc',
+        path:'doc-test/Doc1.docx'
+    },
+    'file3': {
+        filename:'MateralPro.doc',
+        path:'doc-test/Doc1.docx'
+    },
+    'file4': {
+        filename:'MateralPro.doc',
+        path:'doc-test/Doc1.docx'
+    }
+
 }
 
-const app = express();
-app.post('/',(req,res)=>{
-    const {body} = req;
+admin.initializeApp ()
+/* if(process.env.NODE_ENV != 'production'){
+    require('dotenv').config();
+} */
+
+let transporter = nodemailer.createTransport({
+    //host: 'smtp.gmail.com ',
+    //port: 465,
+    service: 'gmail',
+    auth: {
+        user: "rogelio26.dev@gmail.com",
+        pass: "a1e2i3o4u5R"
+    }
+});
+exports.sendMail = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+
+        const {body} = req;
     const isValidMessage = body.message && body.to && body.subject;
-    
-    const fileID = body.fileName;
+
     if(!isValidMessage){
         return res.status(400).send({message: 'invalid request'});
     }
-
-    const transporter = nodemailer.createTransport({
-        //host: 'smtp.gmail.com ',
-        //port: 465,
-        service: 'gmail',
-        auth: {
-            user: "rogelio26.dev@gmail.com",
-            pass: "a1e2i3o4u5R"
-        }
-    });
-
-    const files = {
-        'file1': {
-            filename:'MateralPro.doc',
-            path:'doc-test/Doc1.docx'
-        },
-        'file2': {
-            filename:'MateralPro.doc',
-            path:'doc-test/Doc1.docx'
-        },
-        'file3': {
-            filename:'MateralPro.doc',
-            path:'doc-test/Doc1.docx'
-        },
-        'file4': {
-            filename:'MateralPro.doc',
-            path:'doc-test/Doc1.docx'
-        }
     
-    }
+    const fileID = body.fileName;
 
-    const mailOptions = {
-        from: process.env.EMAIL,
-        to: body.to,
-        subject: body.subject,
-        text: body.message,
-        attachments: [
-            files[fileID],
-        ]
-    }
-
-    transporter.sendMail(mailOptions, 
-        ( err, data)=>{
-            if(err){
-                return res.status(500).send({message: 'Error' + err.message});
-            }
-            return res.send({message:'Correo Enviado'});
-        });
+        const mailOptions = {
+            from: process.env.EMAIL,
+            to: body.to,
+            subject: body.subject,
+            text: body.message,
+            attachments: [
+                files[fileID],
+            ]
+        }
 
 
-})
-app.use(cors());
+        return transporter.sendMail(mailOptions,( err, data)=>{
+                if(err){
+                    return res.status(500).send({message: 'Error' + err.message});
+                }
+                return res.send({message:'Correo Enviado'});
+            });
+    
+    
+    });
+});
 
-exports.mailer = functions.https.onRequest(app);
+
